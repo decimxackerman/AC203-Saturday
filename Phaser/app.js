556ce7e604bae298afbf6f378d2f6e3edd725472
decimxackerman <spychan01@gmail.com>
2017-03-18 12:11:41 +0800

    ...

--- Phaser/app.js
+++ Phaser/app.js
@@ -89,6 +89,7 @@ function update() {
 		for(var i = 0; i<4; i++){
 		var star = stars.create(Math.random()*750, Math.random()*550,'star');
 		star.body.gravity.y = 200;
-		star.body.bounce.y = 0.6 + Math.random()*0.3;
+		star.body.bounce.y = 0.6 + Math.random()*0.3;}
+
 	}
 }
